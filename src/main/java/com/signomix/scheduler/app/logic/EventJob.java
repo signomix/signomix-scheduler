e6ff2d2eb7815b86e1da77cde49f514de9f0d7f6
Grzegorz Skorupa <g.skorupa@gmail.com>
2025-07-18 00:07:20 +0200

    Bump version to 1.6.0; update signomix-common dependency to 1.43.7 and refactor message handling in EventJob for improved clarity and backward compatibility

--- src/main/java/com/signomix/scheduler/app/logic/EventJob.java
+++ src/main/java/com/signomix/scheduler/app/logic/EventJob.java
@@ -1,6 +1,7 @@
 package com.signomix.scheduler.app.logic;
 
 import org.jboss.logging.Logger;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
@@ -20,12 +21,32 @@ public class EventJob extends Job implements  org.quartz.Job {
             logger.error("No channel defined for event job "+id);
             return;
         }
-        String message = (String) context.getMergedJobDataMap().get("message");
+        String message = buildMesage(context.getMergedJobDataMap());
         if(message == null || message.isEmpty()){
             logger.error("No event message defined for event job");
             return;
         }
         logger.info("Sending event : " + channel + " with message: " + message);
         publish(channel, message);
+    }
+
+    private String buildMesage(JobDataMap dataMap) {
+        StringBuilder sb = new StringBuilder();
+        String messageType = dataMap.getString("type");
+        // for backward compatibility
+        if(dataMap.getString("message") != null && !dataMap.getString("message").isEmpty()) {
+            messageType = dataMap.getString("message").toLowerCase();
+        }
+        if (messageType == null || messageType.isEmpty()) {
+            logger.error("No message type defined for event job");
+            return null;
+        }
+        sb.append("type=").append(messageType).append(";");
+        dataMap.forEach((key, value) -> {
+            if (!"message".equals(key) && !"id".equals(key) && !"channel".equals(key) && !"type".equals(key)) {
+                sb.append(key).append("=").append(value).append(";");
+            }
+        });
+        return sb.toString();
     }
 }
